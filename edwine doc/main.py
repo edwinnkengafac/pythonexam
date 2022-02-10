7ee307f43b221ee460d3fc34b3d173acf75da202
Edwine <97540280+edwinnkengafac@users.noreply.github.com>
2022-02-10 12:31:51 +0100

    Add files via upload

---
+++ edwine doc/main.py
@@ -0,0 +1,26 @@
+# so i was unable to compile this into one project,so i did the code for characristics of Cae 1,2 and 3, respectively
+class Car:
+def _init_(self, color,type,year)
+   self.color= "red"
+   self.type= "TOYOTA corolla sedan"
+   self.year = "1999"
+
+   def start(self)
+   return f' "this is" {self.color}{self.type}"from the year"{self.year}'
+
+class Car2:
+def _init_(self, color,type,year)
+   self.color= "white"
+   self.type= "TOYOTA HILUX"
+   self.year = "2010"
+
+   def start(self)
+   return f' "this is" {self.color}{self.type}"from the year"{self.year}'
+class Car3:
+def _init_(self, color,type,year)
+   self.color= "blue"
+   self.type= "Honda CRV"
+   self.year = "2007"
+
+   def start(self)
+   return f' "this is" {self.color}{self.type}"from the year"{self.year}'
